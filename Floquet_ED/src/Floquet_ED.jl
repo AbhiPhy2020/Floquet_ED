65fa3fc1ea9719bfd49c7e3fb1a9c549c35bc7d3
Abhishek Kumar <abhiphys2020@gmail.com>
2024-05-09 23:50:08 -0400

    Created a folder containing Floquet_ED

---
+++ Floquet_ED/src/Floquet_ED.jl
@@ -0,0 +1,15 @@
+module Floquet_ED
+
+include("ED_Floq_Direct.jl")
+#=include("ED_FloqPauliString.jl")
+# #include("Operator_Expt_SVD.jl")
+
+
+# #=
+# To add any additional file I have to include("file.jl") and then I have to export all the functions (that will be used in the main file) in that file here.
+# =#
+export Direct_Floq_Sys,
+Ext_Floq_Sys,
+Eig_Floq_Ham_Square,
+Ext_Floq_Sym_Asym =#
+end
